8319233d837595cffd97b486d19e0532e99f29dd
Corrado Santoro <santoro@dmi.unict.it>
2026-09-24 12:00:55 +0200

    added

--- pong/logic/paddle.gd
+++ pong/logic/paddle.gd
@@ -7,6 +7,7 @@ var _up
 var _down
 
 @onready var _screen_size_y = get_viewport_rect().size.y
+@onready var ball: Area2D = $"../Ball"
 
 func _ready():
 	var n = String(name).to_lower()
@@ -20,9 +21,19 @@ func _ready():
 
 func _process(delta):
 	# Move up and down based on input.
-	var input = Input.get_action_strength(_down) - Input.get_action_strength(_up)
-	position.y = clamp(position.y + input * MOVE_SPEED * delta, 16, _screen_size_y - 16)
-
+	if name == "Right":
+		# RIGHT PADDLE
+		var input = Input.get_action_strength(_down) - Input.get_action_strength(_up)
+		position.y = clamp(position.y + input * MOVE_SPEED * delta, 16, _screen_size_y - 16)
+	else:
+		# LEFT PADDLE
+		var KP = 10.0
+		var target_position = ball.position.y
+		var current_position = position.y
+		var error = target_position - current_position
+		var paddle_speed = error * KP
+		# apply speed to the paddle
+		position.y = position.y + paddle_speed * delta
 
 func _on_area_entered(area):
 	if area.name == "Ball":
